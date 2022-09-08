0db1e9c09000e32571c0051ea32dcabd17a69a68
PedroHenriqueFernandes <pedro007henrique41@gmail.com>
2022-09-08 08:15:25 -0300

    começo da funcionalidade da task

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -2,10 +2,14 @@ import styles from './CreateTask.module.css'
 import img from '../assets/plus.svg'
 
 export function CreateTask(){
+    function handleCreateNewTask(){
+        
+    }
+
     return(
-        <div className={styles.createTask}>
+        <form onSubmit={handleCreateNewTask} className={styles.createTask}>
             <input type="text" placeholder='Adicione uma nova tarefa'/>
             <button>Criar <img src={img} alt="icon plus" /></button>
-        </div>
+        </form>
     )
 }
